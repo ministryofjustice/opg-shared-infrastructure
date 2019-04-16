16f1dd8a24654fddb0fbb63c62599e1693b84941
Dan Webb <dan.webb@damacus.io>
2019-04-16 13:48:37 +0100

    Use the correct s3 state store (#3)

--- credentials.tf
+++ credentials.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "opg.terraform.state"
-    key            = "opg-jenkins/terraform.tfstate"
+    key            = "opg-shared-infrastructure/terraform.tfstate"
     encrypt        = true
     region         = "eu-west-1"
     role_arn       = "arn:aws:iam::311462405659:role/state_write"
