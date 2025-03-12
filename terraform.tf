9f7cb72e16065d041e1e453aea5b26c148a7ed24
Andy Price <sixdaysandy@gmail.com>
2025-03-12 16:36:20 +0000

    SP-2581 - Add VPC Endpoints for Services #minor

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    bucket         = "opg.terraform.state"
-    key            = "opg-shared-infrastructure/terraform.tfstate"
-    encrypt        = true
-    region         = "eu-west-1"
-    role_arn       = "arn:aws:iam::311462405659:role/shared-ci"
+    bucket  = "opg.terraform.state"
+    key     = "opg-shared-infrastructure/terraform.tfstate"
+    encrypt = true
+    region  = "eu-west-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::311462405659:role/shared-ci"
+    }
     dynamodb_table = "remote_lock"
   }
 
